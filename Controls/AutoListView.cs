2804eb7b529112bc3bd694aec873eb2d90aeb6db
DESKTOP-FVQU2FB\DarkD <Dark.Dantari@yandex.ru>
2022-10-02 02:05:14 +0300

    Created AutoListView control. ListViews replaced with AutoListViews.

---
+++ Controls/AutoListView.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Specialized;
+using System.Windows.Controls;
+
+namespace FakeDeleter.Controls
+{
+    internal class AutoListView : ListView
+    {
+        protected override void OnItemsChanged(NotifyCollectionChangedEventArgs e)
+        {
+            base.OnItemsChanged(e);
+
+            if (Items != null && Items.Count > 0)
+            {
+                ScrollIntoView(Items[^1]);
+            }
+        }
+    }
+}
